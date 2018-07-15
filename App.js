29f24e99c2a3b6c31b575dc18356598a8eeb89b9
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-07-15 17:42:22 +0300

    Init project

---
+++ App.js
@@ -0,0 +1,61 @@
+let coursetable = Vue.component('coursetable', {
+  props: ['title', 'courses'],
+  template: `
+  <div>
+  <h2>{{ title }}</h2>
+  <table>
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Name</th>
+        <th scope="col">Org</th>
+        <th scope="col">Dep</th>
+        <th scope="col">level</th>
+        <th scope="col">Tags</th>
+        <th scope="col">Pre-reqs</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="course in courses" :key="course.id">
+          <td>{{ course.courseCode }}</td>
+          <td><a :href="course.url">{{ course.name }}</a></td>
+          <td>{{ course.organiser }}</td>
+          <td>{{ course.department }}</td>
+          <td>{{ course.level}}</td>
+          <td><span v-for="tag in course.tags"><button>{{ tag }}</button></span></td>
+          <td><span v-for="req in course.preReqs"><button>{{ req }}</button></span></td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+  `
+})
+
+new Vue({
+  el: '#app',
+  data: { 
+    courses: [],
+    title: "initial",
+    counter: 0
+  },
+  computed: {
+    progrCourses: function () {
+      return this.courses.filter(course => course.tags.includes("programming"))
+    },
+    uiCourses: function () {
+      return this.courses.filter(course => course.tags.includes("ui"))
+    }
+  },
+  components: { coursetable: coursetable},
+  methods: {
+    showState: function (event) {
+      console.log("courses:", this.courses)
+      console.log("csCourses:", this.csCourses)
+    }
+  },
+  mounted () {
+    axios
+      .get('./assets/courses.json')
+      .then(res => (this.courses = res.data))
+  }
+});
